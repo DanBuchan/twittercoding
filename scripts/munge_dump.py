d0c7fb59795309f1b672630fa91882f455e55de9
Daniel Buchan <clone@myself.com>
2016-06-16 21:27:27 +0100

    coding munge script done

--- scripts/munge_dump.py
+++ scripts/munge_dump.py
@@ -19,20 +19,20 @@ def return_code(category, codes, out_str):
     return(out_str)
 
 tweets = defaultdict(list)
-with open('/Users/dbuchan/Code/twittercoding/test_dump.csv', newline='') as csvfile:
+with open('/Users/dbuchan/Code/twittercoding/code_dump.csv', newline='') as csvfile:
     rows = csv.reader(csvfile)
     for row in rows:
-        print(row[0])
+        # print(row[0])
         tweets[row[0]].append(row)
 
 # pp.pprint(tweets)
-# print("tweet_id,username,label,user,T1: Multimedia,T2: Type of tweet,"
-#       "T3: Interaction with,T4: Is there an @mention?,T5: Source of @mention,"
-#       "T6a: Personal?,T6b: Political?,T7: Personal topic,T8: Political Topic,"
-#
+print("tweet_id,username,label,user,T1: Multimedia,T2: Type of tweet,"
+      "T3: Interaction with,T4: Is there an @mention?,T5: Source of @mention,"
+      "T6a: Personal?,T6b: Political?,T7: Personal topic,T8: Political Topic,")
+
 for tweet_id in tweets:
     codes = tweets[tweet_id]
-    pp.pprint(codes)
+    # pp.pprint(codes)
     out_str = tweet_id+","+tweets[tweet_id][0][1]+","+tweets[tweet_id][0][2] \
               + ","+tweets[tweet_id][0][4]
     out_str = return_code("T1:", codes, out_str)
